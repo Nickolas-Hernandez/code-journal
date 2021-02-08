95460a27dcbe1730f7988321a834e8c7c46a0021
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-08 15:28:33 -0800

    added prevent default, entry object, and updated nextEntryId on data model

--- js/main.js
+++ js/main.js
@@ -2,13 +2,21 @@
 /* exported data */
 
 var $entryForm = document.querySelector('.entry-form');
+var $imageInput = document.querySelector('#image-url');
 
-$entryForm.addEventListener('input', function (event) {
+$imageInput.addEventListener('input', function (event) {
   var $entryImage = document.querySelector('.entry-image');
   $entryImage.setAttribute('src', event.target.value);
 });
 
 $entryForm.addEventListener('submit', function (event) {
   event.preventDefault();
+  var entry = {
+    image: $entryForm.elements.image.value,
+    title: $entryForm.elements.title.value,
+    notes: $entryForm.elements.notes.value,
+    entryId: data.nextEntryId
+  }
+  data.nextEntryId++;
 
 });
