5bb295bff362d47b12fcfdcda0012c95d241f90a
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-08 14:44:12 -0800

    added event listner toimage url input and created function to update image.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+
+var $urlInput = document.querySelector('#image-url');
+var $entryImage = document.querySelector('.entry-image');
+
+$urlInput.addEventListener('input', function (event) {
+  $entryImage.setAttribute('src', event.target.value);
+});
