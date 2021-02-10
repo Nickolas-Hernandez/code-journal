2b8eb8a639a4a7b15b87960f24e1ac99639f7a8c
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-10 10:49:06 -0800

    added image container and resized images for consistency

--- js/main.js
+++ js/main.js
@@ -38,14 +38,17 @@ function createEntry(entry) {
   var entryNotes = document.createElement('p');
   var columnHalf = document.createElement('div');
   var columnOtherHalf = document.createElement('div');
+  var imageContainer = document.createElement('div');
   newEntry.className = 'row';
   columnHalf.className = 'column-half';
   columnOtherHalf.className = 'column-half';
+  imageContainer.className = 'image-container';
   entryImage.setAttribute('src', entry.image);
   entryImage.setAttribute('alt', 'entry image');
   entryTitle.textContent = entry.title;
   entryNotes.textContent = entry.notes;
-  columnHalf.appendChild(entryImage);
+  imageContainer.appendChild(entryImage);
+  columnHalf.appendChild(imageContainer);
   newEntry.appendChild(columnHalf);
   columnOtherHalf.appendChild(entryTitle);
   columnOtherHalf.appendChild(entryNotes);
